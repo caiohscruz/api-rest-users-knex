346e20a9bb796988f4d78e195210827f2375decd
caiohscruz1 <caiohscruz@hotmail.com>
2021-10-25 12:21:06 -0300

    add user model, findEmail and new methods

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const knex = require("../database/connection");
+const bcrypt = require("bcrypt");
+
+class User {
+  async new(name, email, password) {
+    try {
+      var hash = await bcrypt.hash(password, 10);
+      await knex
+        .insert({ name, email, password: hash, role: 0 })
+        .table("users");
+      return true;
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+  }
+
+  async findEmail(email) {
+    try {
+      var result = await knex.select("*").from("users").where({ email: email });
+      if (result.length > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+  }
+}
+
+module.exports = new User();
