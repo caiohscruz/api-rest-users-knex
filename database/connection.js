82622687852ad9820ab1a709370859ce8db7576a
caiohscruz1 <caiohscruz@hotmail.com>
2021-10-25 10:56:13 -0300

    setup connection

---
+++ database/connection.js
@@ -0,0 +1,11 @@
+var knex = require('knex')({
+    client: 'mysql2',
+    connection: {
+      host : 'x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      user : 'do4nc3fd1in3r370',
+      password : 'c8k86xapq6utvf23',
+      database : 'meuw4kyc5zkb4uvc'
+    }
+  });
+
+module.exports = knex
