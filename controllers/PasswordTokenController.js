1d3132f1dba5284a8ec5743b48492200e4b39d87
caiohscruz <caiohscruz@hotmail.com>
2021-10-30 16:11:08 -0300

    add a passowordrecover controller

---
+++ controllers/PasswordTokenController.js
@@ -0,0 +1,18 @@
+const PasswordToken = require("../models/PasswordToken");
+
+class PasswordTokenController {
+    async recoverPassword(req, res) {
+        var email = req.body.email;
+        var result = await PasswordToken.create(email);
+        if (result.status == true) {
+          res.status(200);
+          res.send(result.token);
+        } else {
+          res.status(406);
+          res.send(result.err)
+        }
+      }
+}
+
+module.exports = new PasswordTokenController();
+
