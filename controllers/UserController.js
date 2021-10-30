a3296946020d57df5b25dbc5796a2f67f87b7227
caiohscruz <caiohscruz@hotmail.com>
2021-10-30 16:12:41 -0300

    fix identation

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,46 +3,58 @@ const validator = require('validator');
 
 class UserController {
   async index(req, res) {
-      var users = await User.findAll();
-      res.json(users);
+    var users = await User.findAll();
+    res.json(users);
   }
 
-  async findUser(req, res){
+  async findUser(req, res) {
     var id = req.params.id;
     var user = await User.findById(id);
-    if(user == undefined){
+    if (user == undefined) {
       res.status(404);
       res.json({});
-    }else{
+    } else {
       res.status(200);
       res.json(user);
     }
   }
 
   async create(req, res) {
-    var { name, email, password } = req.body;
+    var {
+      name,
+      email,
+      password
+    } = req.body;
 
     if (name == undefined) {
       res.status(400);
-      res.send({ err: "name não enviado" });
+      res.send({
+        err: "name não enviado"
+      });
       return;
     }
 
     if (email == undefined) {
       res.status(400);
-      res.send({ err: "email não enviado" });
+      res.send({
+        err: "email não enviado"
+      });
       return;
     }
 
-    if(!validator.isEmail(email)){
-        res.status(400);
-        res.send({ err: "email em formato inválido" });
-        return;
+    if (!validator.isEmail(email)) {
+      res.status(400);
+      res.send({
+        err: "email em formato inválido"
+      });
+      return;
     }
 
     if (password == undefined) {
       res.status(400);
-      res.send({ err: "password não enviado" });
+      res.send({
+        err: "password não enviado"
+      });
       return;
     }
 
@@ -50,48 +62,55 @@ class UserController {
 
     if (emailExists) {
       res.status(406);
-      res.json({ err: "email já cadastrado" });
+      res.json({
+        err: "email já cadastrado"
+      });
       return;
     }
 
     var userCreated = await User.new(name, email, password);
 
-    if(userCreated){
-        res.status(200);
-        res.send("Usuário cadastrado");
-    }else{
-        res.status(500);
-        res.send("Houve algum problema interno");
+    if (userCreated) {
+      res.status(200);
+      res.send("Usuário cadastrado");
+    } else {
+      res.status(500);
+      res.send("Houve algum problema interno");
     }
   }
 
-  async edit(req,res){
-    var {id, name, email, role} = req.body;
+  async edit(req, res) {
+    var {
+      id,
+      name,
+      email,
+      role
+    } = req.body;
 
     var result = await User.update(id, name, email, role);
 
-    if(result!=undefined){
-      if(result.status==true){
+    if (result != undefined) {
+      if (result.status == true) {
         res.status(200);
         res.send("Editado com sucesso");
-      }else{
+      } else {
         res.status(406);
         res.send(result.err);
       }
-    }else{
+    } else {
       res.status(406);
       res.send("Ocorreu um erro no servidor");
     }
   }
 
-  async remove(req, res){
+  async remove(req, res) {
     var id = req.params.id;
     var result = await User.delete(id);
 
-    if(result.status==true){
+    if (result.status == true) {
       res.status(200);
       res.send("Deletado com sucesso");
-    }else{
+    } else {
       res.status(406);
       res.send(result.err);
     }
