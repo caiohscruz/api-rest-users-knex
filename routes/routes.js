8d133b8fef212d8407c0514b19c58ba9b7393929
caiohscruz <caiohscruz@hotmail.com>
2021-10-30 16:10:45 -0300

    add route to recover password

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,9 @@
-var express = require("express")
-var app = express();
-var router = express.Router();
-var HomeController = require("../controllers/HomeController");
-var UserController = require("../controllers/UserController");
+const express = require("express")
+const app = express();
+const router = express.Router();
+const HomeController = require("../controllers/HomeController");
+const UserController = require("../controllers/UserController");
+const PasswordTokenController = require("../controllers/PasswordTokenController");
 
 router.get('/', HomeController.index);
 
@@ -15,5 +16,8 @@ router.post("/user", UserController.create);
 router.put("/user", UserController.edit);
 
 router.delete("/user/:id", UserController.remove);
+
+router.post("/recoverpassword", PasswordTokenController.recoverPassword);
+
 
 module.exports = router;
