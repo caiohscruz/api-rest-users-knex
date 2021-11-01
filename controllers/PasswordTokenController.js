06ccbb9908667203d630d5473816c6c019932089
caiohscruz <caiohscruz@hotmail.com>
2021-10-31 22:39:30 -0300

    token is now sent by email

--- controllers/PasswordTokenController.js
+++ controllers/PasswordTokenController.js
@@ -1,12 +1,14 @@
 const PasswordToken = require("../models/PasswordToken");
+const Mailer = require("../nodemailer");
 
 class PasswordTokenController {
   async recoverPassword(req, res) {
     var email = req.body.email;
     var result = await PasswordToken.create(email);
     if (result.status == true) {
+      Mailer(email, result.token);
       res.status(200);
-      res.send(result.token);
+      res.send(`Um email será enviado para ${email}`);
     } else {
       res.status(406);
       res.send(result.err)
@@ -15,4 +17,3 @@ class PasswordTokenController {
 }
 
 module.exports = new PasswordTokenController();
-
